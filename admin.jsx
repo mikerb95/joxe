9f48f8bffb583b193fc789304fe669cd596002b2
Mike <69970540+mikerb95@users.noreply.github.com>
2026-07-18 02:19:44 -0500

    Update available time slots in EmpAbsencesView to include additional evening hours

--- admin.jsx
+++ admin.jsx
@@ -5989,9 +5989,10 @@ const EmpAbsencesView = ({emp}) => {
   const [selectedDate,setSelectedDate] = React.useState(todayStr());
   const [showRangeModal,setShowRangeModal] = React.useState(false);
 
-  const ALL_TIMES = ["9:00","9:30","10:00","10:30","11:00","11:30",
+  const ALL_TIMES = ["10:00","10:30","11:00","11:30",
     "12:00","12:30","13:00","13:30","14:00","14:30",
-    "15:00","15:30","16:00","16:30","17:00","17:30","18:00"];
+    "15:00","15:30","16:00","16:30","17:00","17:30",
+    "18:00","18:30","19:00","19:30","20:00"];
 
   const weekDates = getWeekDates(weekOffset);
   const todayD = todayStr();
