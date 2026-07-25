501d5a55647870ef86184d1996a2cde937fc8665
Mike <69970540+mikerb95@users.noreply.github.com>
2026-07-25 14:14:45 -0500

    Update time options in EmpAbsencesView to use hourly intervals

--- admin.jsx
+++ admin.jsx
@@ -5994,10 +5994,8 @@ const EmpAbsencesView = ({emp}) => {
   const [selectedDate,setSelectedDate] = React.useState(todayStr());
   const [showRangeModal,setShowRangeModal] = React.useState(false);
 
-  const ALL_TIMES = ["10:00","10:30","11:00","11:30",
-    "12:00","12:30","13:00","13:30","14:00","14:30",
-    "15:00","15:30","16:00","16:30","17:00","17:30",
-    "18:00","18:30","19:00","19:30","20:00"];
+  const ALL_TIMES = ["10:00","11:00","12:00","13:00","14:00",
+    "15:00","16:00","17:00","18:00","19:00","20:00"];
 
   const weekDates = getWeekDates(weekOffset);
   const todayD = todayStr();
